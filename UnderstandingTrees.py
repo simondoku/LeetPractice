cf8ff884f9b9b49043979cea96e74e59cd8e64ef
simondoku <sdoku@stetson.edu>
2024-09-22 16:13:35 -0400

    time complexity of remove

--- UnderstandingTrees.py
+++ UnderstandingTrees.py
@@ -4,13 +4,52 @@ class TreeNode:
         self.right = None
         self.left = None
 
-    #searching a binary search tree
-    def search(root, target):
-        if not root:
-            return False
-        if target > root.val:
-            return search(root.right, target)
-        elif target < root.val:
-            return search(root.left, target)
+#searching a binary search tree 
+#Time Complexity O(logn)
+def search(root, target):
+    if not root:
+        return False
+    if target > root.val:
+        return search(root.right, target)
+    elif target < root.val:
+        return search(root.left, target)
+    else:
+        return True
+
+ #time complexity O(logn)  if balanced  
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+    return root
+
+#Time Complexity O(logn) if balanced O(n) if unbalanced
+#Space Complexity O(logn) best Case O(n) worst case
+
+def minValueNode(root):
+    curr = root
+    while curr and curr.left:
+        curr = curr.left
+    return curr
+def remove(root, val):
+    if not root:
+        return None
+    
+    if val > root.val:
+        root.right = remove(root.right, val)
+    elif val < root.val:
+        root.left = remove(root.left, val)
+    else:
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
         else:
-            return True
+            minNode = minValueNode(root.right)
+            root.val = minNode.val
+            root.right = remove(root.right, minNode.val)
+    return root
