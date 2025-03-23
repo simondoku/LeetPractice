1a9da5118c51958e8dae8fcd44f3a5ecc75b0de0
simondoku <sdoku@stetson.edu>
2025-03-23 00:33:51 -0400

    bst insert

--- BinaryTrees/binaryTree.py
+++ BinaryTrees/binaryTree.py
@@ -3,7 +3,7 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
-    #This is a binary tree that is sorted. The left < curr < right
+    # This is a binary tree that is sorted. The left < curr < right
     def binarySearchTree(self, root, target):
         if not root:
             return False
@@ -13,3 +13,13 @@ class TreeNode:
             return self.binarySearchTree(root.left, target)
         else:
             return True
+    # To insert a new node into a BST, we must ensure that we maintain the l<c<r property.
+    def insert(self, root, val):
+        #If the tree is empty, insert the value as the first value of the tree(root)
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insert(root.left, val)
+        elif val > root.val:
+            root.right = self.insert(root.right, val)
+        return root
